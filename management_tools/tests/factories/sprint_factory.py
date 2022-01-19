b3d5fe570594b78ac02bd38fc0f15c95e7202754
oscarfa <oscarfernandezangulo@gmail.com>
2022-01-19 16:23:46 +0100

    Añadido el nombre en SprintFactory.

--- management_tools/tests/factories/sprint_factory.py
+++ management_tools/tests/factories/sprint_factory.py
@@ -4,16 +4,18 @@ from management_tools.sprint import Sprint
 class SprintFactory:
     def __new__(
             cls,
+            name="Sprint name",
             programmers_count=1,
             hours_per_point=1,
-            start_date='5/9/21',
-            pre_deployment_date='14/9/21',
-            end_date='16/9/21',
+            start_date='6/9/21',
+            pre_deployment_date='15/9/21',
+            end_date='17/9/21',
             support_percentage=30,
             pre_deployment_days=2,
             issues_df=None
     ):
         return Sprint(
+            name=name,
             programmers_count=programmers_count,
             hours_per_point=hours_per_point,
             start_date=start_date,
