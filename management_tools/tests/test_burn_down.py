8afc2e74b13cf80632c4e4be49a01d89c02e7a1d
oscarfa <oscarfernandezangulo@gmail.com>
2022-01-19 16:18:12 +0100

    Actualizados los tests del burndown.

--- management_tools/tests/test_burn_down.py
+++ management_tools/tests/test_burn_down.py
@@ -12,6 +12,7 @@ class TestBurnDown(TestCase):
     def setUp(self) -> None:
         self._init_past_sprint()
         self._init_current_sprint()
+        self._init_current_sprint_with_issues_on_progress()
 
     def _init_past_sprint(self):
         issues_dict = {
@@ -20,7 +21,8 @@ class TestBurnDown(TestCase):
             'Cerrada': [
                 '2021-5-03 12:00', '2021-5-04 12:00', '2021-5-05 12:00', '2021-5-06 12:00',
                 '2021-5-07 12:00', '2021-5-10 12:00', '2021-5-11 12:00',
-            ]
+            ],
+            'Estado': ['Cerrada'] * 7
         }
         issues_df = pandas.DataFrame.from_dict(issues_dict)
         issues_df['Cerrada'] = pandas.to_datetime(issues_df['Cerrada'])
@@ -38,7 +40,8 @@ class TestBurnDown(TestCase):
             'Prioridad': ['Alta'] * 7,
             'Cerrada': [
                 '2021-5-03 12:00', '2021-5-04 12:00', '2021-5-05 12:00',
-            ] + 4 * [None]
+            ] + 4 * [None],
+            'Estado': ['Cerrada', 'Cerrada', 'Cerrada', 'Nueva', 'Nueva', 'Nueva', 'Nueva']
         }
         issues_df = pandas.DataFrame.from_dict(issues_dict)
         issues_df['Cerrada'] = pandas.to_datetime(issues_df['Cerrada'])
@@ -50,7 +53,26 @@ class TestBurnDown(TestCase):
             issues_df=issues_df
         )
 
-    def test_burn_down_past_sprint(self):
+    def _init_current_sprint_with_issues_on_progress(self):
+        issues_dict = {
+            'Story points': [1] * 7,
+            'Prioridad': ['Alta'] * 7,
+            'Cerrada': [
+                '2021-5-03 12:00', '2021-5-04 12:00',
+            ] + 5 * [None],
+            'Estado': ['Cerrada', 'Cerrada', 'En curso', 'En curso', 'Resuelta', 'Comentarios', 'Nueva']
+        }
+        issues_df = pandas.DataFrame.from_dict(issues_dict)
+        issues_df['Cerrada'] = pandas.to_datetime(issues_df['Cerrada'])
+
+        self.current_sprint_with_issues_on_progress = SprintFactory(
+            start_date='3/5/21',
+            pre_deployment_date='12/5/21',
+            end_date='14/5/21',
+            issues_df=issues_df
+        )
+
+    def test_past_sprint(self):
         fig, ax = plt.subplots()
 
         self.past_sprint.plot_burndown(fig, ax)
@@ -58,9 +80,17 @@ class TestBurnDown(TestCase):
         fig.show()
 
     @freeze_time("2021-05-05")
-    def test_burn_down_current_sprint(self):
+    def test_current_sprint(self):
         fig, ax = plt.subplots()
 
         self.current_sprint.plot_burndown(fig, ax)
+
+        fig.show()
+
+    @freeze_time("2021-05-05")
+    def test_current_sprint_with_issues_on_progress(self):
+        fig, ax = plt.subplots()
+
+        self.current_sprint_with_issues_on_progress.plot_burndown(fig, ax)
 
         fig.show()
